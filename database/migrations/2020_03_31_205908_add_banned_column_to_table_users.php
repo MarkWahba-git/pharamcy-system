04e6fca2d799b480acfc86fc43b3448a1e26e87d
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-04-01 00:58:39 +0200

    some changes in database

--- database/migrations/2020_03_31_205908_add_banned_column_to_table_users.php
+++ database/migrations/2020_03_31_205908_add_banned_column_to_table_users.php
@@ -14,7 +14,7 @@ class AddBannedColumnToTableUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('banned');
+            $table->boolean('is_banned');
         });
     }
 
