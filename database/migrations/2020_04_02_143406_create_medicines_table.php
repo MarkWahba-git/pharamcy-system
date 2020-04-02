a43647e5e4343dc2181b753041578c7c937fd418
Nohier <nohierassan2@gmail.com>
2020-04-02 18:12:38 +0200

    Adding the medicine migrations

---
+++ database/migrations/2020_04_02_143406_create_medicines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->id();
+            $table->string('durg_name');
+            $table->string('drug_type');
+            $table->float('drug_unit_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicines');
+    }
+}
