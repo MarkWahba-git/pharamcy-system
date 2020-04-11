c77adff4687ab1594a862111d877103652d03dbf
AndrewNagyNemr <58911021+AndrewNagyNemr@users.noreply.github.com>
2020-04-11 13:21:56 +0200

    resolving conflict in table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-=======
             $table->string('nat_id');
             $table->string('name');
             $table->string('email')->unique();
@@ -32,7 +26,6 @@ class CreateUsersTable extends Migration
             $table->string('role')->nullable();
             $table->binary('avatar');
             $table->integer('is_banned')->default(0);
->>>>>>> Dev
             $table->rememberToken();
             $table->timestamps();
         });
